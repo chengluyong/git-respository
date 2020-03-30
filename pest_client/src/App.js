4381ae7c79917f33e74232787383907e5583dd38
chengluyong <2317697255@qq.com>
2020-03-30 15:30:44 +0800

    app router

--- pest_client/src/App.js
+++ pest_client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { BrowserRouter, Route } from "react-router-dom";
+import {HashRouter} from 'react-router-dom';
+import {CacheSwitch, CacheRoute} from 'react-cache-router';
 import QueryReport from "./corpus/QueryReport";
 import QueryData from "./corpus/QueryData";
+import QuerySevereData from "./corpus/QuerySevereData";
 import QueryView from "./corpus/QueryView";
 import Link from "react-router-dom/Link";
 
@@ -11,36 +13,40 @@ class App extends React.Component
 {
   render()
   {
-    return  <BrowserRouter>
-          <div  className="row" style={ { margin: '0', padding: '0px'} }>
+    return  <HashRouter >
+      <div  className="row" style={ { margin: '0', padding: '0px'} }>
 
-            <div  className="col-2" style={ { margin: '0', padding: '0px'} }>
-              <div style={ { margin: 0, padding: '0px'} }>
-                  <div className="list-group" role="tablist">
-                    <Link to={ '/data' }>
-                      <button className={'list-group-item'} style={ {'width': '100%', color: '', borderRadius: '0', backgroundColor: '\t#F3F3FA'} }>数据视图</button>
-                    </Link>
-                    <Link to={ '/chart' }>
-                      <button className={'list-group-item '} style={ {'width': '100%', color: '', borderRadius: '0', backgroundColor: '\t#F3F3FA'} }>统计分析</button>
-                    </Link>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-10" style={ {  padding: '0px'} }>
-              <Route path={ '/' } exact ={ true } component = { ()=> <QueryData /> }/>
-
-              <Route path={ '/data' } exact ={ true } component = { ()=> <QueryData /> }/>
-
-              <Route path={ '/chart' } exact ={ true } component = { ()=> <QueryView /> }/>
-
-              <Route path={ '/report/:name' } exact={ true } component=
-                  {
-                    ({ match })=> <QueryReport name = { match.params.name }/>
-                  } />
+        <div  className="col-2" style={ { margin: '0', padding: '0px'} }>
+          <div style={ { margin: 0, padding: '0px'} }>
+            <div className="list-group" role="tablist">
+              <Link to={ '/' }>
+                <button className={'list-group-item'} style={ {'width': '100%', color: '', borderRadius: '0', backgroundColor: '\t#F3F3FA'} }>数据视图</button>
+              </Link>
+              <Link to={ '/chart' }>
+                <button className={'list-group-item '} style={ {'width': '100%', color: '', borderRadius: '0', backgroundColor: '\t#F3F3FA'} }>统计分析</button>
+              </Link>
+              <Link to={ '/severe' }>
+                <button className={'list-group-item '} style={ {'width': '100%', color: '', borderRadius: '0', backgroundColor: '\t#F3F3FA'} }>重症数据</button>
+              </Link>
             </div>
           </div>
-      </BrowserRouter>;
+        </div>
+
+        <div className="col-10" style={ {  padding: '0px',background: '#F8F8FF' } }>
+          <CacheSwitch>
+
+              <CacheRoute  path={ '/' } exact ={ true } component = { QueryData }/>
+
+              <CacheRoute  path={ '/chart' } exact ={ true } component = { QueryView  }/>
+
+              <CacheRoute  path={ '/report' } exact={ true } component={ QueryReport } />
+
+              <CacheRoute  path={ '/severe' } exact={ true } component={ QuerySevereData } />
+
+            </CacheSwitch>
+        </div>
+      </div>
+    </HashRouter >;
   }
 }
 
